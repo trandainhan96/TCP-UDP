5fd1bea39c1dfa6a47c35d5aa315539b7cf1b006
trandainhan96 <dainhantran160396@gmail.com>
2017-10-05 12:51:17 +0700

    updata

--- UDPSocket-Client/Program.cs
+++ UDPSocket-Client/Program.cs
@@ -14,7 +14,18 @@ namespace UDPSocket_Client
         {
             Console.WriteLine("Khoi tao Client ");
             //b1 xac dinh ip server
-            IPEndPoint
+            IPEndPoint ipServer = new IPEndPoint(IPAddress.Parse("127.0.0.1"), 12345);
+            //b2 Tao socket
+            Socket client = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
+            //b3 gui nhan data
+            byte[] send = Encoding.ASCII.GetBytes("Hello Server!");
+            EndPoint epServer = (EndPoint)ipServer;
+            client.SendTo(Send, epServer);
+            byte[] rec = new byte[25];
+            client.ReceiveFrom(rec, ref epServer);
+            Console.WriteLine("Client nhan data {0}", Encoding.ASCII.GetString(rec));
+            //b4 dong Client Socket
+            client.Close();
         }
     }
 }
